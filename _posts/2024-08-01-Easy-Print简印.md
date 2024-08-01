a0d549f2e98ad540bf1f421cc08e78e801c49895
jan3540 <jjt3540358681@gmail.com>
2024-08-01 17:54:37 +0800

    East-Print

---
+++ _posts/2024-08-01-Easy-Print简印.md
@@ -0,0 +1,57 @@
+---
+layout: mypost
+title: Easy-Print简印
+categories: [Python，脚本]
+---
+
+# Easy-Print--简印
+
+Easy-Print是一个用于打印指定目录中文件及其内容的 Python 脚本。支持排除指定的文件或目录，并将结果输出到指定的输出文件中。
+
+## 功能特点
+
+- 递归列出指定目录中的文件及其内容。
+- 支持排除指定的文件或目录。
+- 将文件路径和内容输出到指定的输出文件中。
+
+## 安装python3
+
+```bash
+yum -y insatll python3
+python3 --version
+```
+-------
+
+
+## 用法
+
+使用以下命令格式运行脚本：
+
+```bash
+Easy-Print.py
+```
+也可以指定参数
+```bash
+Easy-Print.py -d  <dir_copy>-n <file_or_directory_to_exclude> -o <output_file_name>
+```
+## 添加到环境变量（任何路径都可以使用脚本）
+
+```bash
+echo 'export PATH=$PATH:/root/Easy-Print' >> ~/.bashrc
+source ~/.bashrc
+```
+## Github主页  
+[Easy-Print](https://github.com/jan3540/Easy-Print "Easy-Print")  
+
+## linux用法
+![image](Usage_linux.png)
+
+## windows用法
+![image](Usage_windows.png)  
+
+
+[Easy-Print.py下载](Easy-Print.py)
+
+---
+
+<p align="right">作于2024年08月01日</p>
---
+++ posts/2024/08/01/Easy-Print.py
@@ -0,0 +1,61 @@
+
+#!/usr/bin/env python3
+import os
+import sys
+import argparse
+
+def list_files(directory, exclude, output_file):
+    # 获取当前脚本的文件名
+    script_name = os.path.basename(__file__)
+
+    # 打开输出文件
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for root, dirs, files in os.walk(directory):
+            # 过滤掉排除的目录和以 '.' 开头的目录
+            dirs[:] = [d for d in dirs if os.path.join(root, d) not in exclude and not d.startswith('.')]
+            for file_name in files:
+                if file_name.startswith('.'):
+                    continue
+                file_path = os.path.join(root, file_name)
+                if file_path not in exclude and file_name != script_name:
+                    f.write("{}\n".format(file_path))
+                    f.write("*********************\n")
+                    try:
+                        # 以 UTF-8 编码读取文件内容
+                        with open(file_path, 'r', encoding='utf-8') as file:
+                            f.write(file.read())
+                            f.write("\n\n")
+                    except (UnicodeDecodeError, OSError):
+                        try:
+                            # 尝试以 GBK 编码读取文件内容
+                            with open(file_path, 'r', encoding='gbk') as file:
+                                f.write(file.read())
+                                f.write("\n\n")
+                        except Exception as e:
+                            f.write("Error reading file: {}\n\n".format(e))
+
+def main():
+    # 创建参数解析器
+    parser = argparse.ArgumentParser(description="列出文件及其内容")
+    parser.add_argument('-d', '--directory', default='.', help="开始列出文件的目录")
+    parser.add_argument('-n', '--no-print', nargs='+', default=[], help="要排除的文件或目录")
+    parser.add_argument('-o', '--output-file', default='easy-print.txt', help="输出文件名")
+
+    # 解析参数
+    args = parser.parse_args()
+
+    output_file = args.output_file
+    directory = os.path.abspath(args.directory)
+    exclude = [os.path.abspath(item) for item in args.no_print]
+
+    # 调用 list_files 函数
+    list_files(directory, exclude, output_file)
+    print("输出已写入到 {}".format(output_file))
+
+if __name__ == "__main__":
+    # 没有参数时执行默认行为
+    if len(sys.argv) == 1:
+        list_files(os.getcwd(), [], 'easy-print.txt')
+        print("输出已写入到 easy-print.txt")
+    else:
+        main()
